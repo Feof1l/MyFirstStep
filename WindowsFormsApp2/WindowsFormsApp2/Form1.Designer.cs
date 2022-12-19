2424d8c92dc021c5acadaf9cdd44c3cd240508d1
Feof1l <121018313+Feof1l@users.noreply.github.com>
2022-12-19 12:30:47 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(1011, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 84);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Убрать круги";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1011, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 84);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Кнопка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.button2_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1011, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(257, 84);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Круги ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Paint += new System.Windows.Forms.PaintEventHandler(this.button3_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(743, 506);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 66);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 32);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "игрок 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(693, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 142);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Tag = "";
+            this.groupBox1.Text = "Кто играет крестиком";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 104);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 32);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "игрок 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 641);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        bool first = true;
+        int[,] m = new int[5, 5];
+        bool flag = false;
+        int dx;
+        int dy;
+        bool emblem = false;
+        
+        TextureBrush texture1;
+        TextureBrush texture2;
+        private List<Button> listButtons = new List<Button>();
+
+        static void DrawEmblem(Graphics g,Button But)
+        {
+            //Graphics g = Graphics.FromHwnd(But.Handle);
+            int width = But.Width;
+            int height = But.Height;
+            g.FillEllipse(new SolidBrush(Color.Gold), width / 3 +10 , height / 3 - 10 , width / 2  , height / 2 );
+            g.FillRectangle(new SolidBrush(Color.White), width / 3 + 24, height / 3 , width / 3  + 4, height / 3 - 17);
+            g.FillRectangle(new SolidBrush(Color.Blue), width / 3 + 24, height / 3 +5, width / 3 + 4, height / 3 - 17 );
+            g.FillRectangle(new SolidBrush(Color.Red), width / 3 + 24, height / 3 + 9, width / 3 + 4, height / 3 - 17);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            listButtons.Add(button1);
+            listButtons.Add(button2);
+            listButtons.Add(button3);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (emblem)
+            {
+                DrawEmblem(e.Graphics, button2);
+            }
+            /* Graphics g = Graphics.FromHwnd(button1.Handle);
+             int width = button1.Width;
+             int height = button1.Height;
+             g.FillEllipse(new SolidBrush(Color.Gold), width / 3 + 10, height / 3 - 10, width / 2, height / 2);
+             g.FillRectangle(new SolidBrush(Color.White), width / 3 + 24, height / 3, width / 3 + 4, height / 3 - 17);
+             g.FillRectangle(new SolidBrush(Color.Blue), width / 3 + 24, height / 3 + 5, width / 3 + 4, height / 3 - 17);
+             g.FillRectangle(new SolidBrush(Color.Red), width / 3 + 24, height / 3 + 9, width / 3 + 4, height / 3 - 17);*/
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        { 
+            //foreach (Button x in listButtons)
+            //    DrawEmblem(x);
+            if (flag)
+            {
+                e.Graphics.DrawEllipse(new Pen(Color.Blue, 3), 550, 250, 50, 50);
+                e.Graphics.DrawEllipse(new Pen(Color.Yellow, 3), 580, 270, 50, 50);
+                e.Graphics.DrawEllipse(new Pen(Color.Black, 3), 610, 250, 50, 50);
+                e.Graphics.DrawEllipse(new Pen(Color.Green, 3), 640, 270, 50, 50);
+                e.Graphics.DrawEllipse(new Pen(Color.Red, 3), 670, 250, 50, 50);
+                e.Graphics.DrawLine(new Pen(Color.Blue, 3), 600, 279, 600, 275);
+                e.Graphics.DrawLine(new Pen(Color.Black, 3), 660, 269, 660, 275);
+                e.Graphics.DrawLine(new Pen(Color.Green, 3), 669, 270, 673, 270);
+                e.Graphics.DrawLine(new Pen(Color.Yellow, 3), 608, 269, 613, 272);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Invalidate();
+            flag = false;
+        }
+
+        private void button3_Paint(object sender, PaintEventArgs e)
+        {
+            if (emblem)
+            {
+                DrawEmblem(e.Graphics, button2);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+
+        {
+            Image myimage = Image.FromFile("D:\\Sistym Prog\\LR6_1\\LR6\\WindowsFormsApp2\\WindowsFormsApp2\\im.bmp");
+            TextureBrush myTextureBrush = new TextureBrush(myimage);
+            Color newcolor = Color.FromArgb(150, Color.MediumAquamarine);
+            SolidBrush brush = new SolidBrush(newcolor);
+
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+           
+
+
+
+            dx = width / 5 ;
+            dy = height / 5 ;
+            
+            Pen p = new Pen(Color.Black, 1);
+            e.Graphics.FillRectangle(myTextureBrush, 0, 0, width, height);
+            e.Graphics.FillRectangle(brush, 0, 0, width, height);
+            
+            e.Graphics.DrawLine(p, 0, 0, 0, height);
+            e.Graphics.DrawLine(p, 0, 0, width , 0);
+            for (int i = 1; i < 6; ++i)
+            {
+                e.Graphics.DrawLine(p, dx * i -1, 0, dx * i-1, height -1);
+                e.Graphics.DrawLine(p, 0, dy * i -1, width -1, dy * i -1);
+            }
+            
+        }
+        public static bool IsFull(int[,] m)
+        {
+            for (int i = 0; i <= m.GetUpperBound(0); i++)
+                for (int j = 0; j <= m.GetUpperBound(1); j++)
+                    if (m[i, j] == 0)
+                        return false;
+            return true;
+        }
+        private int Winner()
+        {
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 2; j++)
+                    if (m[i, j] > 0)
+                    {
+                        for (int k = j + 1; k < 5; k++)
+                        {
+                            if (m[i, k] != m[i, j])
+                                break;
+                            else if (k - j == 3)
+                                if (m[i, j] == 1)
+                                    return 1;
+                                else
+                                    return 2;
+                        }
+                    }
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 5; j++)
+                    if (m[i, j] > 0)
+                    {
+                        for (int k = i + 1; k < 5; k++)
+                        {
+                            if (m[k, j] != m[i, j])
+                                break;
+                            else
+                            if (k - i == 3)
+                                if (m[i, j] == 1)
+                                    return 1;
+                                else
+                                    return 2;
+                        }
+                    }
+            return 0;
+        } 
+
+        private void pictureBox1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+                int x = e.X / (dx+1);
+                int y = e.Y / (dy+1);
+
+            if (m[x, y] == 0)
+            {
+                if (radioButton1.Checked == true)
+                {
+                    Image image1 = Image.FromFile("D:\\Sistym Prog\\LR6_1\\LR6\\WindowsFormsApp2\\WindowsFormsApp2\\1.png");
+                    texture1 = new TextureBrush(image1);
+                    Image image2 = Image.FromFile("D:\\Sistym Prog\\LR6_1\\LR6\\WindowsFormsApp2\\WindowsFormsApp2\\2.png");
+                    texture2 = new TextureBrush(image2);
+                }
+                else
+                {
+                    Image image1 = Image.FromFile("D:\\Sistym Prog\\LR6_1\\LR6\\WindowsFormsApp2\\WindowsFormsApp2\\2.png");
+                    texture1 = new TextureBrush(image1);
+                    Image image2 = Image.FromFile("D:\\Sistym Prog\\LR6_1\\LR6\\WindowsFormsApp2\\WindowsFormsApp2\\1.png");
+                    texture2 = new TextureBrush(image2);
+                }
+                Graphics gr = pictureBox1.CreateGraphics();
+                Rectangle r = new Rectangle(x * dx, y * dy, dx, dy);
+                if (first)
+                {
+                    LinearGradientBrush br = new LinearGradientBrush(r, Color.PowderBlue, Color.Lime, LinearGradientMode.Vertical);
+                    gr.FillRectangle(br, r);
+                    gr.FillRectangle(texture1, r);
+                    m[x, y] = 1;
+                }
+                else
+                {
+                    LinearGradientBrush br = new LinearGradientBrush(r, Color.Indigo, Color.HotPink, LinearGradientMode.Vertical);
+                    gr.FillRectangle(br, r);
+                    gr.FillRectangle(texture2, r);
+                    m[x, y] = 2;
+                }
+
+                int winner = Winner();
+                if (winner != 0)
+                {
+                    label1.Text = "Победил " + (winner == 1 ? "первый " : "второй ") + "игрок.";
+                    radioButton1.Enabled = true;
+                    radioButton2.Enabled = true;
+                    Refresh();
+                    for (int i = 0; i <= m.GetUpperBound(0); i++)
+                        for (int j = 0; j <= m.GetUpperBound(1); j++)
+                            m[i, j] = 0;
+                    first = true;
+                }
+                else if (IsFull(m))
+                {
+                    label1.Text = "Боевая ничья";
+                    radioButton1.Enabled = true;
+                    radioButton2.Enabled = true;
+                    Refresh();
+                    for (int i = 0; i <= m.GetUpperBound(0); i++)
+                        for (int j = 0; j <= m.GetUpperBound(1); j++)
+                            m[i, j] = 0;
+                    first = true;
+                    
+                     
+                    
+                    
+                }
+                else
+                {
+                    first = !first;
+                    radioButton1.Enabled = false;
+                    radioButton2.Enabled = false;
+
+
+
+                }
+                
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //var hWnd = this.Handle;
+            Invalidate();
+            flag = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           // flag = true;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Paint(object sender, PaintEventArgs e)
+        {
+            if (emblem)
+            {
+                DrawEmblem(e.Graphics,button2);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            emblem = !emblem;
+            foreach (Button x in listButtons)
+                x.Refresh();
+        }
+    }
+}
